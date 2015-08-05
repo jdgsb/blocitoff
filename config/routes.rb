a735d7620ba10eff31629af97af8a929e7ba9f81
jdgsb <uber.tubers@gmail.com>
2015-08-04 19:02:04 -0700

    basic user profile only email

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   get 'welcome/index'
 
+  get '/welcome' => "users#show", as: :user_root
 
   devise_for :users
   resource :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
